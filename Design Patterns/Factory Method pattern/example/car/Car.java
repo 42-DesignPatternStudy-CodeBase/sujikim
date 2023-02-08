0535e4f8183aa0cee0905e13449454257e44c6c9
sujikim <sujikim@student.42seoul.kr>
2023-02-08 20:12:37 +0900

    ADD: Factory Method pattern 과제 해결 코드 car 추가

---
+++ Design Patterns/Factory Method pattern/example/car/Car.java
@@ -0,0 +1,8 @@
+public abstract class Car {
+
+    String productName;
+    
+    public String toString() {
+        return productName;
+    }
+}
---
+++ Design Patterns/Factory Method pattern/example/car/HyundaiCar.java
@@ -0,0 +1,6 @@
+public class HyundaiCar extends Car {
+    
+    public static final String SONATA = "Sonata";
+    public static final String GRANDEUR = "Grandeur";
+    public static final String GENESIS = "Genesis";
+}
