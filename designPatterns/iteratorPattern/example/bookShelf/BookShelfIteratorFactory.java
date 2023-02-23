1ae59636ce23e9725c02cd5c963c241d36552e7f
sujikim <sujikim@student.42seoul.kr>
2023-02-23 14:41:54 +0900

    FIX: Iterator bookShelf 오타 수정

--- designPatterns/iteratorPattern/example/bookShelf/BookShelfIteratorFactory.java
+++ designPatterns/iteratorPattern/example/bookShelf/BookShelfIteratorFactory.java
@@ -2,7 +2,7 @@ package iteratorPattern.example.bookShelf;
 
 public class BookShelfIteratorFactory extends Factory {
     public static final int NORMAL = 0;
-    public static final int REVERSE = 0;
+    public static final int REVERSE = 1;
 
     protected Iterator createProduct(Aggregate list, int type) {
         return type == NORMAL ? list.createIterator() : list.createReverseIterator();
