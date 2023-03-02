c457387cf1951ea94c5463cbd3e19476f45d11e2
sujikim <sujikim@student.42seoul.kr>
2023-03-02 15:11:25 +0900

    FIX: Decorate pattern coffee 예제 타입 오류 해결 최상위 타입인 Coffee로 변경

--- designPatterns/decoratorPattern/example/coffee/_Main.java
+++ designPatterns/decoratorPattern/example/coffee/_Main.java
@@ -1,20 +1,21 @@
 package decoratorPattern.example.coffee;
 
 public class _Main {
+    
     public static void main(String[] args) {
         Bean kenya = new Bean("Kenya");
         Bean etiopia = new Bean("Etiopia");
 
-        Americano order1 = new Americano(kenya);
+        Coffee order1 = new Americano(kenya);
         System.out.println(order1.getName());
 
-        Americano order2 = new Milk(order1);
+        Coffee order2 = new Milk(order1);
         System.out.println(order2.getName());
 
-        Americano order3 = new MochaSyrup(order2);
+        Coffee order3 = new MochaSyrup(order2);
         System.out.println(order3.getName());
         
-        Americano order4 = new Americano(etiopia);
+        Coffee order4 = new Americano(etiopia);
         order4 = new Milk(order4);
         order4 = new MochaSyrup(order4);
         order4 = new WhippedCream(order4);
